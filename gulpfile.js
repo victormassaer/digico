777f97e17bd2deb339f7a9944a52502f7c3eba23
Aaron Claes <aaronclaes22@gmail.com>
2021-04-28 12:24:23 +0200

    feat: add nodemon to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
-const { src, dest, watch } = require('gulp');
+const gulp = require("gulp");
+const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass");
+const nodemon = require("gulp-nodemon");
 sass.compiler = require("sass");
 
-sass2css = function(done){
-    return src('./src/sass/app.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(dest('public/stylesheets/'))
-    done();
-}
+sass2css = function (done) {
+  return src("./src/sass/app.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(dest("public/stylesheets/"));
+  done();
+};
 
-exports.default = function() {
-    // You can use a single task
-    watch('./src/sass/**/*.scss', sass2css);
-  };
+exports.default = function () {
+  // You can use a single task
+  watch("./src/sass/**/*.scss", sass2css);
+  //   watch(nodemon);
+  nodemon({
+    script: "app.js",
+    ext: "js html",
+    env: { NODE_ENV: "development" },
+  });
+};

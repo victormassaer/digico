a221ccc44dfa61fc8d4f4196b8753a29dac9c79f
Aaron Claes <aaronclaes22@gmail.com>
2021-05-05 11:36:44 +0200

    feat: add parameters to user model

--- models/User.js
+++ models/User.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name: String,
+  firstName: String,
+  lastName: String,
+  username: String,
+  email: String,
+  password: String,
   coins: Number,
+  created: Date,
 });
 
 const User = mongoose.model("User", userSchema);

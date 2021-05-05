faeae73eebe13c044715b38f393aabc4d0e4a81e
Aaron Claes <aaronclaes22@gmail.com>
2021-05-05 11:41:47 +0200

    feat: add parameters to users controller

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -3,8 +3,13 @@ const { get } = require("../../../routes/api/v1/users");
 
 const createUser = (req, res) => {
   let user = new User();
-  user.name = req.body.name;
+  user.firstName = req.body.firstName;
+  user.lastName = req.body.lastName;
+  user.username = req.body.username;
+  user.email = req.body.email;
+  user.password = req.body.password;
   user.coins = req.body.coins;
+  user.created = Date.now();
   user.save((err, doc) => {
     if (err) {
       res.json({

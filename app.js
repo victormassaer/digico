dad241c79909fa2505be34e16a632569f706af7e
Aaron Claes <aaronclaes22@gmail.com>
2021-05-12 19:34:58 +0200

    fix: rename frontend route to pages route

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const transfersRouter = require("./routes/api/v1/transfers");
 const usersRouter = require("./routes/api/v1/users");
-const frontendRouter = require("./routes/frontend");
+const pagesRouter = require("./routes/pages");
 
 mongoose.connect(
   `mongodb+srv://admin:${process.env.DB_PASS}@digico.vjbbh.mongodb.net/digico?retryWrites=true&w=majority`,
@@ -36,15 +36,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use("/api/v1/transfers", transfersRouter);
 app.use("/api/v1/users", usersRouter);
-app.use("/frontend", frontendRouter);
+app.use("/pages", pagesRouter);
 
 //pages
-app.get("/index", frontendRouter);
-app.get("/signup", frontendRouter);
-app.get("/signin", frontendRouter);
-app.get("/transfer", frontendRouter);
-app.get("/login", frontendRouter);
-app.get("/feed", frontendRouter);
+app.get("/index", pagesRouter);
+app.get("/signup", pagesRouter);
+app.get("/signin", pagesRouter);
+app.get("/transfer", pagesRouter);
+app.get("/login", pagesRouter);
+app.get("/feed", pagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

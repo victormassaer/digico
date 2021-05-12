6c6a78e43be69e32588f763a19c457708b876aa7
Aaron Claes <aaronclaes22@gmail.com>
2021-05-12 12:43:21 +0200

    feat: add routes for html pages

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ db.once("open", function () {
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -37,16 +37,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+// app.use("/", indexRouter);
 app.use("/api/v1/transfers", transfersRouter);
 app.use("/api/v1/users", usersRouter);
 app.use("/frontend", frontendRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "public/index.html");
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(__dirname + "/public/signup.html");
+});
+
+app.get("/signin", (req, res) => {
+  res.sendFile(__dirname + "/public/signin.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html");
+});
+
+app.get("/feed", (req, res) => {
+  res.sendFile(__dirname + "/public/feed.html");
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
-// error handler
+// // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -54,7 +74,11 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  // res.render("error");
+});
+
+app.listen(3001, function () {
+  console.log("Server is running on port 3001");
 });
 
 module.exports = app;

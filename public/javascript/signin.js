8bafed2a10c573925fb269267d0984512ccfb837
Aaron Claes <aaronclaes22@gmail.com>
2021-05-19 11:51:15 +0200

    feat: link signin frontend to backend with fetch

--- public/javascript/signin.js
+++ public/javascript/signin.js
@@ -1,25 +1,26 @@
-document.querySelector("#signin").addEventListener("click", () =>{
-    let email = document.querySelector("#email").value;
-    let password = document.querySelector("password").value;
+document.querySelector("#signin").addEventListener("click", () => {
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("password").value;
 
-    fetch('HIER KOMT VERDERE ROUTE', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-        }),
+  fetch("HIER KOMT VERDERE ROUTE", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        }).then(json => {
-            if(json.status === "succes") {
-                console.log("Sign in complete");
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    .then((json) => {
+      if (json.status === "succes") {
+        console.log("Sign in complete");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,31 +1,31 @@
-document.querySelector("#btnLogin").addEventListener("click", () => {
-    let firstName = document.querySelector("#firstName").value;
-    let lastName = document.querySelector("#lastName").value;
-    let username = document.querySelector("#username").value;
-    let email = document.querySelector("#email").value;
-    let password = document.querySelector("#password").value;
-
-    fetch('HIER KOMT VERDERE ROUTE', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "firstName": firstName,
-            "lastName": lastName,
-            "username": username,
-            "email": email,
-            "password": password
-        }),
+document.querySelector("#btnSignup").addEventListener("click", (e) => {
+  e.preventDefault();
+  let firstName = document.querySelector("#firstName").value;
+  let lastName = document.querySelector("#lastName").value;
+  let username = document.querySelector("#username").value;
+  let email = document.querySelector("#email").value;
+  let password = document.querySelector("#password").value;
+  console.log(username, password);
+  fetch(`http://localhost:3000/api/v1/users/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  })
+    .then((response) => {
+      return response.json();
     })
-        .then(response => {
-            return response.json();
-        }).then(json => {
-            if(json.status === "succes") {
-                console.log("Sign up complete");
-            }
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    .then((json) => {
+      if (json.status === "succes") {
+        console.log("Sign up complete");
+        window.location.href = "../feed.html";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });

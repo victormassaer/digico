405400829d60e229ebd84d50233e000cd8a7f860
Aaron Claes <aaronclaes22@gmail.com>
2021-05-19 17:41:45 +0200

    fix: merge conflicts

--- public/javascript/signin.js
+++ public/javascript/signin.js
@@ -3,7 +3,6 @@ document.querySelector("#btnSignin").addEventListener("click", (e) => {
   let username = document.querySelector("#username").value;
   let password = document.querySelector("#password").value;
 
-<<<<<<< HEAD
   fetch("http://localhost:3001/api/v1/users/login", {
     method: "POST",
     headers: {
@@ -16,17 +15,6 @@ document.querySelector("#btnSignin").addEventListener("click", (e) => {
   })
     .then((response) => {
       return response.json();
-=======
-    fetch('http://localhost:3000/frontend/signin', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "email": email,
-            "password": password
-        }),
->>>>>>> fe75df2d20cf365d8d7e67cd8c6ece61597f0793
     })
     .then((json) => {
       if (json.status === "succes") {

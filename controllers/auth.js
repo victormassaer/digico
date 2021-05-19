b634553fc7410da239dada4fc1d6fc94180a6b05
Aaron Claes <aaronclaes22@gmail.com>
2021-05-19 10:25:37 +0200

    feat: pass additional signup info to mongodb

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,19 @@ const User = require("../models/User");
 const signup = async (req, res, next) => {
   let username = req.body.username;
   let password = req.body.password;
-  const user = new User({ username: username });
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+  let coins = req.body.coins;
+  let created = Date.now();
+  const user = new User({
+    username: username,
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    coins: coins,
+    created: created,
+  });
   await user.setPassword(password);
   await user
     .save()
@@ -15,6 +27,7 @@ const signup = async (req, res, next) => {
     .catch((error) => {
       res.json({
         status: "error",
+        message: error.message,
       });
     });
 };
--- models/User.js
+++ models/User.js
@@ -2,19 +2,13 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const passportLocalMongoose = require("passport-local-mongoose");
 
-// const userSchema = new Schema({
-//   firstName: String,
-//   lastName: String,
-//   username: String,
-//   email: String,
-//   password: String,
-//   coins: Number,
-//   created: Date,
-// });
-
-// const User = mongoose.model("User", userSchema);
-
-const User = new Schema({});
+const User = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  coins: Number,
+  created: Date,
+});
 User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", User);

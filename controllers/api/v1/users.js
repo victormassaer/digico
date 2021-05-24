4f5b9ad0628f9414ddfae73bca8a59c025d41080
Aaron Claes <aaronclaes22@gmail.com>
2021-05-24 20:52:51 +0200

    feat: link leaderboard with api

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -2,21 +2,23 @@ const User = require("../../../models/User");
 const { get } = require("../../../routes/api/v1/users");
 
 function getLeaderboard(req, res) {
-  User.find({}, (err, doc) => {
-    if (err) {
-      res.json({
-        status: "error",
-        message: "Could not find users",
-      });
-    }
-    if (!err) {
-      res.json({
-        status: "succes",
-        message: "GETTING LEADERBOARD",
-        data: { user: doc.coins },
-      });
-    }
-  });
+  User.find({})
+    .sort({ coins: "descending" })
+    .exec((err, doc) => {
+      if (err) {
+        res.json({
+          status: "error",
+          message: "Could not find users",
+        });
+      }
+      if (!err) {
+        res.json({
+          status: "succes",
+          message: "GETTING LEADERBOARD",
+          data: { users: doc },
+        });
+      }
+    });
 }
 
 module.exports.getLeaderboard = getLeaderboard;
---
+++ public/javascript/leaderboard.js
@@ -0,0 +1,28 @@
+fetch("http://localhost:3000/api/v1/users/leaderboard", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((result) => {
+    if (result.status === "succes") {
+      const list = document.querySelector(".list");
+
+      users = result.data.users;
+      users.forEach((user) => {
+        let person = document.createElement("LI");
+        person.classList.add("person");
+        item = `<p><span class="name">${user.firstName} ${user.lastName}</span> - <span class="coins">${user.coins} coins</span></p>`;
+        person.innerHTML = item;
+        list.appendChild(person);
+      });
+    } else {
+      console.log("request failed");
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });

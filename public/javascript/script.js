4d3122564efc4e27408139d75d0b438d0feec572
Aaron Claes <aaronclaes22@gmail.com>
2021-05-26 09:51:18 +0200

    feat: add hamburger script to script.js

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,22 +1,25 @@
-// fetch("http://localhost:3000/auth/loggedin", {
-//   method: "GET",
-//   headers: {
-//     "Content-Type": "application/json",
-//     Authorization: "Bearer " + localStorage.getItem("token"),
-//   },
-// })
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((result) => {
-//     if (result.status === "succes") {
-//     } else {
-//       console.log("request failed");
-//     }
-//   })
-//   .catch((error) => {
-//     window.location.href = "login";
-//   });
+let clicked = 0;
+
+document.querySelector(".nav a").addEventListener("click", (e) => {
+  let navigation = document.querySelector(".navigation");
+  e.preventDefault;
+
+  switch (clicked) {
+    case 0:
+      navigation.style = "display: block;";
+      console.log(clicked + "display block");
+      clicked = 1;
+      break;
+
+    case 1:
+      navigation.style = "display: none;";
+      console.log(clicked);
+      clicked = 0;
+      break;
+  }
+});
+
+//get current user and set balance
 const id = localStorage.getItem("id");
 fetch(`http://localhost:3000/api/v1/users/user/${id}`, {
   method: "GET",
